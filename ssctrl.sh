89f96e2d9c538cf5e25b84481d806c12cdbe5805
jfcarr <jfcarr@gmail.com>
2017-08-19 21:47:09 -0400

    Changed multiple if statements to switch/case

--- ssctrl.sh
+++ ssctrl.sh
@@ -52,42 +52,39 @@ if [ $# -ne 1 ]; then
 	usage
 fi
 
-if [ $1 == "status" ]; then
-	statusmsg $1
-	systemctl status mssql-server
-	exit
-fi
-
-if [ $1 == "start" ]; then
-	statusmsg $1
-	sudo systemctl start mssql-server
-	exit
-fi
-
-if [ $1 == "stop" ]; then
-	statusmsg $1
-	sudo systemctl stop mssql-server
-	exit
-fi
-
-if [ $1 == "restart" ]; then
-	statusmsg $1
-	sudo systemctl restart mssql-server
-	exit
-fi
-
-if [ $1 == "disable" ]; then
-	statusmsg $1
-	sudo systemctl stop mssql-server
-	sudo systemctl disable mssql-server
-	exit
-fi
-
-if [ $1 == "enable" ]; then
-	statusmsg $1
-	sudo systemctl enable mssql-server
-	sudo systemctl start mssql-server
-	exit
-fi
+case $1 in
+	status)
+		statusmsg $1
+		systemctl status mssql-server
+		exit
+		;;
+	start)
+		statusmsg $1
+		sudo systemctl start mssql-server
+		exit
+		;;
+	stop)
+		statusmsg $1
+		sudo systemctl stop mssql-server
+		exit
+		;;
+	restart)
+		statusmsg $1
+		sudo systemctl restart mssql-server
+		exit
+		;;
+	disable)
+		statusmsg $1
+		sudo systemctl stop mssql-server
+		sudo systemctl disable mssql-server
+		exit
+		;;
+	enable)
+		statusmsg $1
+		sudo systemctl enable mssql-server
+		sudo systemctl start mssql-server
+		exit
+		;;
+esac
 
 usage
